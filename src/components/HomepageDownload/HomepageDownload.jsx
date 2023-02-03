898e1f915e890687fda8689ed4d0e692faa09f10
Bob Hall <bhall_2001@yahoo.com>
2023-02-03 08:46:40 -0500

    feat: updated for v5.0.0

--- src/components/HomepageDownload/HomepageDownload.jsx
+++ src/components/HomepageDownload/HomepageDownload.jsx
@@ -7,7 +7,7 @@ export default function HomepageDownload() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__subtitle">Download Flyball Manager</h1>
-        <p>v21.2.2</p>
+        <p>v5.0.0</p>
         <div className="buttons_src-components-HomepageDownload-styles-module">
           <a
             href={
