797671c7c6180d07be2dd396e0f95e0a93d030e4
Bob Hall <bhall_2001@yahoo.com>
2025-07-31 07:53:10 -0400

    chore: update to version 3.6.1, increase pricing to $15 per day

--- src/components/HomepageDownload/HomepageDownload.jsx
+++ src/components/HomepageDownload/HomepageDownload.jsx
@@ -7,23 +7,13 @@ export default function HomepageDownload() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className='container'>
         <h1 className='hero__subtitle'>Download Flyball Manager</h1>
-        <p>
-          🚧 Heads up! We've discovered a bug that we haven't seen before, and
-          it’s causing some issues. To keep things running smoothly for
-          everyone, we've temporarily disabled the download links while we work
-          on identifying and resolving it.
-        </p>
-        <p>
-          Thanks so much for your patience! Once everything is fixed, the
-          download links will be back right here. Last updated: 2025-07-28
-        </p>
-        {/* <p>v6.3.0</p>
-        <div className="buttons_src-components-HomepageDownload-styles-module">
+        <p>v6.3.1</p>
+        <div className='buttons_src-components-HomepageDownload-styles-module'>
           <a
             href={
               'https://s3.us-east-2.amazonaws.com/downloads.flyballmanager.com/Flyball_Manager_Setup.exe'
             }
-            className="button button--secondary button--lg"
+            className='button button--secondary button--lg'
           >
             Windows
           </a>
@@ -32,7 +22,7 @@ export default function HomepageDownload() {
             href={
               'https://s3.us-east-2.amazonaws.com/downloads.flyballmanager.com/Flyball_Manager-Apple.dmg'
             }
-            className="button button--secondary button--lg"
+            className='button button--secondary button--lg'
           >
             macOS Apple Silicon
           </a>
@@ -41,7 +31,7 @@ export default function HomepageDownload() {
             href={
               'https://s3.us-east-2.amazonaws.com/downloads.flyballmanager.com/Flyball_Manager-Intel.dmg'
             }
-            className="button button--secondary button--lg"
+            className='button button--secondary button--lg'
           >
             macOS Intel
           </a>
@@ -51,7 +41,7 @@ export default function HomepageDownload() {
           Give Flyball Manager a try by downloading the app! There's no charge
           to explore and test it out. A fee only applies if you decide to use it
           for scoring a tournament.
-        </p>  */}
+        </p>
       </div>
     </header>
   );
